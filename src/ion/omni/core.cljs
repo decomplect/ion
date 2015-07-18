fc98bcdef36e45c49ae4519f6e85140731a22c68
Patrick K. O'Brien <patrick.keith.obrien@gmail.com>
2015-07-18 08:38:19 -0500

    cleanup

--- src/ion/omni/core.cljs
+++ src/ion/omni/core.cljs
@@ -1,12 +1,6 @@
 (ns ion.omni.core
-  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require
-   [cljs.core :as cljs]
-   [cljs.core.async :refer [<! >! chan close! put! sliding-buffer timeout]]))
+   [ion.poly.core :as poly]))
 
-;(enable-console-print!)
+(enable-console-print!)
 
-
-(defn spin [& opts]
-  ;(prn "omni/spin" opts)
-  true)
